d780e99db691fa2a9fe4fda9671dc7069aec03ae
Jane <jane624609@gmail.com>
2019-12-10 20:28:08 +0800

    補充

--- 密碼重試.py
+++ 密碼重試.py
@@ -1,7 +1,7 @@
 #密碼輸入練習，若超過三次則強制結束
 password = 'a123456' #先存正確密碼
 x = 2
-while True:
+while True:   #補充1- 也可以換while x > 0: ，這樣補充二那也不用驗證 i == 0，i = i - 1也可以挪到前面
     pwd = input ('請輸入密碼: ')
     if pwd == password:
         print('登入成功')
@@ -14,6 +14,7 @@ while True:
             break
         x = x - 1
 
+#補充二
 #else那段可以再縮減
     #else:
         #i = i - 1
