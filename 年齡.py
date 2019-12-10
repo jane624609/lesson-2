08e40d0d087c938b6f75ff3589600ab29f88a4df
Jane <jane624609@gmail.com>
2019-12-10 14:33:23 +0800

    add country

--- 年齡.py
+++ 年齡.py
@@ -1,11 +1,17 @@
 #輸入年齡、國家，判斷可否開車
 country = input('請問你是哪國人:')
-age = input('你輸日年齡:')
+age = input('你輸入年齡:')
 age = int(age)
 if country == '台灣':
     if  age >= 18:
         print('你可以考駕照')
     else:
+        print('你還不能考駕照')        
+elif country == '美國':
+    if  age >= 16:
+        print('你可以考駕照')
+    else:
         print('你還不能考駕照')
-        
-elif
+
+else:
+    print('只能輸入 台灣/美國')
