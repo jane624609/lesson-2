9f96828af9ec18919f61c29d633143d133e2858e
Jane <jane624609@gmail.com>
2019-12-10 14:24:42 +0800

    Age estimation

---
+++ 年齡.py
@@ -0,0 +1,11 @@
+#輸入年齡、國家，判斷可否開車
+country = input('請問你是哪國人:')
+age = input('你輸日年齡:')
+age = int(age)
+if country == '台灣':
+    if  age >= 18:
+        print('你可以考駕照')
+    else:
+        print('你還不能考駕照')
+        
+elif
