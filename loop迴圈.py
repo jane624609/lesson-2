991e27ec6d229bb91b0e4508001988f0e69512c6
Jane <jane624609@gmail.com>
2019-12-10 15:22:58 +0800

    增加迴圈補充

--- loop迴圈.py
+++ loop迴圈.py
@@ -6,3 +6,25 @@ while x < 10:
     x = x + 1 #當不小於就會停止迴圈
 
 print('我逃出迴圈了')
+
+
+#break
+x = 5
+while True:#因為永遠是true，迴圈會無限下去
+    print ('x小於10喔!')
+    print('我還困在框框裡')
+    break #強制跳出迴圈來終止
+print('我逃出迴圈了')
+
+
+#遊戲模式選擇
+while True:
+    mode = input('請輸入遊戲模式: ')
+    if mode == 'q': #quit
+        break #只有寫這才會離開
+    elif mode == '1':
+        print('啟動模式一')
+    elif mode == '2':
+        print('啟動模式二')
+    else:
+        print('只能輸入 1/2/q')
