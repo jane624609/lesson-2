d6ee3dcf23c24e01d81c42fe82956b843206862f
Jane <jane624609@gmail.com>
2019-12-17 09:59:45 +0800

    使用os.path.isfile()來檢查檔案在不在

--- 記帳程式.py
+++ 記帳程式.py
@@ -1,3 +1,27 @@
+#檢查要讀取的檔案是否存在
+#使用os.path.isfile()來檢查檔案在不在    #此句解釋:os模組裡面的path路徑模組的isfile檢查檔案在不在的功能
+import os  #import是載入，os是operating system作業系統
+
+products = []
+if os.path.isfile('products.csv'):  #路徑有分絕對路徑跟相對路徑，因為目前要找的檔案跟此程式檔存放位置相同，就用相對路徑放檔名就好，如果要找的文件在不同資料夾，就要用絕對路徑，清楚寫出在甚麼槽的甚麼檔案裡(完整路徑)
+    print('yeha! 找到檔案了!')
+
+    #讀取檔案(將讀到的寫入)
+    #用strip移除換行符號\n
+    #用split(',')做逗點分割
+    #用continue使標題(商品，價格)避開不要也匯入，意思是直接跳過到下一回圈
+    with open('products.csv', 'r') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue #繼續，這次迴圈不執行，直接跳到下一回
+            name, price= line.strip().split(',')
+            products.append([name, price])
+    print(products)
+
+else:
+    print('找不到檔案....')
+####################################################################
+
 #讀取檔案(將讀到的寫入)
 #用strip移除換行符號\n
 #用split(',')做逗點分割
@@ -10,6 +34,8 @@ with open('products.csv', 'r') as f:
         name, price= line.strip().split(',')
         products.append([name, price])
 print(products)
+
+##################################################################
 
 #記帳程式，讓使用者多次輸入購買品項
 #二維度清單(清單內再裝清單)2 dimensional
@@ -27,7 +53,11 @@ while  True:
 
     products.append(p) #或是p直接改products.append([name, price])
 print (products )
+
+####################################################################
+
 #練習存取二維清單
+#印出所有購買紀錄
 print (products[0][0])  #第一個大清單裡的第一個(name)
 
 #for loop搞清楚每個東西是甚麼
@@ -39,6 +69,8 @@ for p in products:
 
 for p in products:
     print(p[0], '的價格是', p[1])  #所有大清單的第一個跟小清單的第二個位置
+
+###################################################################################
 
 #字串可以做加乘，但減跟除不能
 #但加法只能字串跟字串或整數跟整數，不能整數跟字串
